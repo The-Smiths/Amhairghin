9648e2fa1a6ccb2ff342f3b16604fad6baf9312f
ClinkMakesGames <32272073+ClinkMakesGames@users.noreply.github.com>
2018-10-26 16:35:20 +0100

    Serialized gridmap to under new pathfinder script, added editor script for pathfinder script

--- Assets/Code/Pathfinding/GridMap.cs
+++ Assets/Code/Pathfinding/GridMap.cs
@@ -2,48 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridMap : MonoBehaviour
+[ExecuteInEditMode]
+[System.Serializable]
+public class GridMap
 {
-    [SerializeField] private LayerMask _obstructionMask;
-    [SerializeField] private Vector2 _gridSize;
+    [HideInInspector] public Node[,] grid; // [HideInInspector] attribute is unnecesary as Node isn't serializable, but I'll add it anyway
+    [HideInInspector] public float nodeRadius = 0.5f;
+
+    public LayerMask obstructionMask;
+    public Vector2 gridSize;
+    public Vector3 gridCenter;
     
-    private float _nodeRadius = 0.5f;
-    private Node[,] _grid;
-
-    private void Start()
+    public void CreateGrid()
     {
-        CreateGrid();
-    }
+        grid = new Node[(int) gridSize.x, (int) gridSize.y];
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireCube(transform.position, new Vector3(_gridSize.x, _gridSize.y, 1));
+        Vector3 bottomLeft = gridCenter - (Vector3.right * gridSize.x / 2) - (Vector3.up * gridSize.y / 2);
 
-        if (_grid != null)
+        for (int x = 0; x < grid.GetLength(0); x++)
         {
-            foreach (Node node in _grid)
+            for (int y = 0; y < grid.GetLength(1); y++)
             {
-                Gizmos.color = node.obstruction ? Color.blue : Color.red;
-                Gizmos.DrawCube(node.position, Vector3.one * (_nodeRadius * 2 - 0.1f));
+                Vector3 position = bottomLeft + Vector3.right * (x + nodeRadius) + Vector3.up * (y + nodeRadius);
+                bool obstruction = !Physics2D.OverlapCircle(position, nodeRadius, obstructionMask);
+
+                grid[x, y] = new Node(obstruction, position);
             }
         }
     }
 
-    private void CreateGrid()
+    public Node WorldPositionToNode(Vector3 position)
     {
-        _grid = new Node[(int) _gridSize.x, (int) _gridSize.y];
+        float percentX = Mathf.Clamp01((position.x + gridSize.x / 2) / gridSize.x);
+        float percentY = Mathf.Clamp01((position.y + gridSize.y / 2) / gridSize.y);
 
-        Vector3 bottomLeft = transform.position - (Vector3.right * _gridSize.x / 2) - (Vector3.up * _gridSize.y / 2);
+        int x = (int) Mathf.Round((gridSize.x - 1) * percentX);
+        int y = (int) Mathf.Round((gridSize.y - 1) * percentY);
 
-        for (int x = 0; x < _grid.GetLength(0); x++)
-        {
-            for (int y = 0; y < _grid.GetLength(1); y++)
-            {
-                Vector3 position = bottomLeft + Vector3.right * (x + _nodeRadius) + Vector3.up * (y + _nodeRadius);
-                bool obstruction = !Physics2D.OverlapCircle(position, _nodeRadius, _obstructionMask);
-
-                _grid[x, y] = new Node(obstruction, position);
-            }
-        }
+        return grid[x, y];
     }
 }
---
+++ Assets/Code/Pathfinding/Pathfinder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public GridMap grid;
+
+    private void Start()
+    {
+        grid.CreateGrid();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(grid.gridCenter, new Vector3(grid.gridSize.x, grid.gridSize.y, 1));
+
+        if (grid.grid != null)
+        {
+            foreach (Node node in grid.grid)
+            {
+                Gizmos.color = node.obstruction ? Color.blue : Color.red;
+                Gizmos.DrawCube(node.position, Vector3.one * (grid.nodeRadius * 2 - 0.1f));
+            }
+        }
+    }
+}
