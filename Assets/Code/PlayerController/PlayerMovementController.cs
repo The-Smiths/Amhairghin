a7a72c03a2891327af8825b86d271e98949b5b7e
Tiuna <jeremi2323@gmail.com>
2018-10-27 20:39:08 +0200

    Added crude player prefab with movement controller

---
+++ Assets/Code/PlayerController/PlayerMovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float Speed = 40f;
+
+    private float m_hInput = 0;
+    private bool m_isJumping = false;
+    private bool m_isCrouching = false;
+
+    private void Update()
+    {
+        m_hInput = Input.GetAxisRaw("Horizontal");
+        m_isJumping = Input.GetButtonDown("Jump");
+        m_isCrouching = Input.GetButton("Crouch");
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(m_hInput * Speed * Time.fixedDeltaTime, m_isCrouching, m_isJumping);
+        if (m_isJumping)
+            Debug.Log("SPACE PRESSED");
+    }
+}
