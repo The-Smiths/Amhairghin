595b1590fabf257ad10819a3cd8ddf52230f8d24
Tiuna <jeremi2323@gmail.com>
2018-10-30 20:33:58 +0100

    TargetFollow for camera and little tweaks for ground and ceiling check for player

---
+++ Assets/Code/CameraUtils/TargetFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFollow : MonoBehaviour 
+{
+    public Transform Target;
+    public float SmoothSpeed = 6.25f;
+    public Vector3 Offset = new Vector3(0,0,-10);
+
+    private void FixedUpdate()
+    {
+        var alpha = SmoothSpeed * Time.deltaTime;
+        var newPosition = Vector3.Lerp(transform.position, Target.position, alpha);
+
+        transform.position = newPosition + Offset;
+    }
+}
