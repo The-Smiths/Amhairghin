b9349390d80a8f8b097cfb89ac57ec50a4fca900
Tiuna <jeremi2323@gmail.com>
2018-10-31 15:21:36 +0100

    Review fixed for camera TargetFollow script

--- Assets/Code/CameraUtils/TargetFollow.cs
+++ Assets/Code/CameraUtils/TargetFollow.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class TargetFollow : MonoBehaviour 
 {
     public Transform Target;
-    public float SmoothSpeed = 6.25f;
-    public Vector3 Offset = new Vector3(0,0,-10);
+    [SerializeField]private float SmoothSpeed = 6.25f;
+    [SerializeField]private Vector3 Offset = new Vector3(0,0,-10);
 
     private void FixedUpdate()
     {
-        var alpha = SmoothSpeed * Time.deltaTime;
-        var newPosition = Vector3.Lerp(transform.position, Target.position, alpha);
+        float interpolant = SmoothSpeed * Time.deltaTime;
+        Vector3 newPosition = Vector3.Lerp(transform.position, Target.position, interpolant);
 
         transform.position = newPosition + Offset;
     }
